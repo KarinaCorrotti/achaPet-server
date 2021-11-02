d6483406309b73e2665db1b89aad7a521ff0cdd9
kakinha <karina.corrotti@gmail.com>
2021-11-02 15:25:19 -0300

    atualizando

--- routes/users.js
+++ routes/users.js
@@ -116,7 +116,7 @@ router.put('/updateUser', verifyJWT, async(req, res) => {
 
 
 router.put('/putFoto', verifyJWT, multer(multerConfig).single("file"), async(req, res) =>{   
-  console.log(req) 
+  console.log(req.body) 
   const { originalname: nomeFoto, size: tamanho, key, location: url = ""} = req.file;
   try{    
     const user = await User.findOneAndUpdate(
